1ff95de8c51b4bd912b3c7ac70c24f2896d961c6
tamas-polgar <tamas.polgar.dev@gmail.com>
2019-04-20 20:28:14 +0200

    --

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { en_US, NgZorroAntdModule, NZ_I18N, NzI18nModule } from 'ng-zorro-antd';
+import { en_US, NgZorroAntdModule, NZ_I18N } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ReactiveFormsModule, NgZorroAntdModule, NzI18nModule],
+  imports: [CommonModule, ReactiveFormsModule, NgZorroAntdModule],
 
   providers: [{ provide: NZ_I18N, useValue: en_US }],
-  exports: [NgZorroAntdModule, NzI18nModule, ReactiveFormsModule],
+  exports: [NgZorroAntdModule, ReactiveFormsModule],
 })
 export class SharedModule {}
